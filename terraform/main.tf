431b47c705ac287a6466570d830ce28ed15c9e37
ikram98ai <ikram98ai@gmail.com>
2025-07-29 17:11:04 +0500

    aurora-postgresql v15.4 doesn't support auto pause

--- terraform/main.tf
+++ terraform/main.tf
@@ -196,7 +196,7 @@ resource "aws_rds_cluster" "aurora" {
   cluster_identifier              = "${var.function_name}-aurora-cluster"
   engine                          = "aurora-postgresql"
   engine_mode                     = "provisioned"
-  engine_version                  = "14.5"
+  engine_version                  = "16.8"
   database_name                   = var.db_name
   master_username                 = var.db_username
   master_password                 = var.db_password
