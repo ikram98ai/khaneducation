6194e39f3d6b797c43bb797749029a19a9310d1d
ikram98ai <ikram98ai@gmail.com>
2025-08-04 23:53:17 +0500

    adding deployment script to npm

--- src/services/api.ts
+++ src/services/api.ts
@@ -19,7 +19,7 @@ import {
 
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8000";
+const API_BASE_URL = "https://api.khaneducation.ai" //"http://localhost:8000";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
